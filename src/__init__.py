1df03fe7161c92e59c8a2749e6464d81dee16d88
edwinvice48-hue <edwinvice48@gmail.com>
2026-06-20 12:44:23 +0300

    Add __init__.py for src package

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+Hotel Booking Analysis Package
+Main module initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Hotel Analysis Team"
+
+from . import data_processing
+from . import analysis
+from . import visualization
+
+__all__ = ['data_processing', 'analysis', 'visualization']
